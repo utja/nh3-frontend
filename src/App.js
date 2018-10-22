27c9edfa590099560c1aec08bb17d32ef63adb10
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-21 21:19:04 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Test
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+      </Router>
     );
   }
 }
