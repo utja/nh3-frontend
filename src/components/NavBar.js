430f2516d47ec6e23f6f365ac828a701029980b2
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-23 19:27:55 -0400

    change color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import '../assets/css/NavBar.css'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = props => {
   return(
-    <div className="navbar">Nav Bar</div>
+    <div className="navbar">
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/">Home</NavLink>
+    </div>
+
+//     <div class="navbar">
+//   <a href="#home">Home</a>
+//   <a href="#news">News</a>
+//   <div class="dropdown">
+//     <button class="dropbtn">Dropdown 
+//       <i class="fa fa-caret-down"></i>
+//     </button>
+//     <div class="dropdown-content">
+//       <a href="#">Link 1</a>
+//       <a href="#">Link 2</a>
+//       <a href="#">Link 3</a>
+//     </div>
+//   </div> 
+// </div>
   )
 }
 
