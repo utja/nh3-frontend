e849c0b99aa50b4057425c77347ffc1af32ada23
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-23 21:25:09 -0400

    add onclick to open search

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,43 @@ import React from 'react'
 import '../assets/css/NavBar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const NavBar = props => {
-  const handleClick = () => {
-    console.log('hello')
+class NavBar extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isUserFocusing: false,
+    }
   }
-  return(
+
+  // componentDidMount() {
+  //   document.body.addEventListener('click', this.handleClickOutside);
+  // }
+
+  // componentWillUnmount() {
+  //   document.body.removeEventListener('click', this.handleClickOutside);
+  // }
+
+  handleClick = () => {
+    this.setState({isUserFocusing: !this.state.isUserFocusing})
+  }
+
+  // handleClickOutside(event) {
+  //   debugger
+  //   if (!document.body.contains(event.target)) {
+  //     console.log('outside click');
+  //   }
+  // }
+
+  render(){
+    return(
     <div className="navbar">
-      <button onClick={handleClick}>
+      <button onClick={this.handleClick}>
         <FontAwesomeIcon icon="search" color="white" size="lg" />
+        {this.state.isUserFocusing ? "SEARCH BAR" : null }
       </button>
     </div>
-  )
+    )
+  }
 }
 
 export default NavBar
