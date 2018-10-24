d51e5adc289be1dbcf3b29b06c103ef6f6ca1150
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-10-23 20:14:46 -0400

    remove react icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../assets/css/NavBar.css'
 import { NavLink } from 'react-router-dom'
-import { FaSearch } from "react-icons/fa";
 
 const NavBar = props => {
   const handleClick = () => {
@@ -11,23 +10,7 @@ const NavBar = props => {
     <div className="navbar">
       <NavLink to="/about">About</NavLink>
       <NavLink to="/">Home</NavLink>
-      <FaSearch color="white" onClick={handleClick} />
     </div>
-
-//     <div class="navbar">
-//   <a href="#home">Home</a>
-//   <a href="#news">News</a>
-//   <div class="dropdown">
-//     <button class="dropbtn">Dropdown 
-//       <i class="fa fa-caret-down"></i>
-//     </button>
-//     <div class="dropdown-content">
-//       <a href="#">Link 1</a>
-//       <a href="#">Link 2</a>
-//       <a href="#">Link 3</a>
-//     </div>
-//   </div> 
-// </div>
   )
 }
 
