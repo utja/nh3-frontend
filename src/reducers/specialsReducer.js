ae828e795c083c8cbb48f72e2b0fd00d2e6b81b2
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-11-02 21:16:51 -0400

    load all specials

--- src/reducers/specialsReducer.js
+++ src/reducers/specialsReducer.js
@@ -1,14 +1,19 @@
+import * as TYPES from '../types/specials'
+
 const initialState = {
   specials: [],
   selectedSpecial: null,
+  loadingSpecials: false,
 }
 
 export default function specialsReducer(state = initialState, action) {
   switch (action.type) {
-    case "TEST":
-      return Object.assign({}, state, {
-        selectedSpecial: action.payload
-      })
+    case TYPES.LOADING_SPECIALS:
+      return {...state, loadingSpecials: true}
+    case TYPES.SET_SPECIALS:
+      return {...state, specials: action.payload}
+    case TYPES.LOADED_SPECIALS:
+      return {...state, loadingSpecials: false}
     default:
       return state
   }
