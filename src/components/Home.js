5687b4b18ff14014b2a6ee13542e3fedbeca3520
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-11-02 21:17:53 -0400

    add all specials

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import * as constants from '../constants'
+import React from 'react';
+import * as constants from '../constants';
+import SpecialsContainer from './specials/SpecialsContainer';
 
 class Home extends React.Component {
 
   render(){
-    let now = new Date()
-    let today = now.getDay()
+    let now = new Date();
+    let today = now.getDay();
     return(
       <div>
         <h1>Happy {constants.WEEKDAYS[today]}!</h1>
+        <SpecialsContainer day={today}/>
       </div>
     )
   }
