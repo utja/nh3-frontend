414337456ab0d1197b244c09b6e14ea00fa0da5d
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-11-15 01:07:09 -0500

    add toggle for special

--- src/components/specials/SpecialCard.js
+++ src/components/specials/SpecialCard.js
@@ -22,15 +22,25 @@ class SpecialCard extends React.Component {
     return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
   }
 
+  handleClick = event => {
+    this.setState({toggled: !this.state.toggled})
+  }
+
   render(){
     console.log(this.props);
     return (
-      <div className="special">
+      <div className="special" onClick={this.handleClick}>
+        {this.state.toggled ? 
+        <ul>
+          <li>Back of card</li>
+        </ul>
+        :
         <ul>
           <li>{this.props.special.restaurant.name}</li>
           <li>Special: {this.props.special.name}</li>
           <li>Time: {this.msToTime(Date.parse(this.props.special.start_time))} - {this.msToTime(Date.parse(this.props.special.end_time))}</li>
-        </ul>
+        </ul> 
+        }
       </div>
     )
   }
